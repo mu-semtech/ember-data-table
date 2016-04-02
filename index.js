29979304a056d3cd6f2247a27b1f12029c3c4ac6
Erika Pauwels <erika.pauwels@tenforce.com>
2016-04-02 22:49:58 +0200

    Add context menu component

--- index.js
+++ index.js
@@ -2,5 +2,20 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-mu-datatable'
+  name: 'ember-mu-datatable',
+  included: function(app) {
+    this._super.included(app);
+
+    if (app.import) {
+      this.importDependencies(app);
+    }
+  },
+  importDependencies: function(app) {
+    app.import('vendor/tether/tether.js');
+  },
+  isDevelopingAddon: function() {
+    return true;
+  }
 };
+
+
