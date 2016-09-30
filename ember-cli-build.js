6d80376292fc3c52defbd87275093494863697f4
Erika Pauwels <erika.pauwels@gmail.com>
2016-09-30 22:52:37 +0200

    Upgrade ember-cli-materialize

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,16 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+var nodeSass = require('node-sass'); // loads the version in your package.json
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
+    // Add options here
     sassOptions: {
-      includePaths: ['bower_components/materialize/sass']
+      includePaths: [
+        'bower_components/materialize/sass'
+      ],
+      nodeSass: nodeSass // Workaround for ember-cli-sass bug https://github.com/aexmachina/ember-cli-sass/issues/117
     }
   });
 
