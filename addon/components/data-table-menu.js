37724b177e2be06d419b21771523ff3d7e9e5307
Erika Pauwels <erika.pauwels@gmail.com>
2016-10-25 17:45:21 +0200

    Fix styling of data-table header menu
    Apply the selected class when items are selected in the table.

--- addon/components/data-table-menu.js
+++ addon/components/data-table-menu.js
@@ -4,7 +4,7 @@ import layout from '../templates/components/data-table-menu';
 export default Ember.Component.extend({
   layout,
   classNames: ['data-table-header'],
-  classNameBindings: ['selectionIsEmpty:selected'],
+  classNameBindings: ['selectionIsEmpty::selected'],
   selectionIsEmpty: Ember.computed('selection', function() {
     return this.get('selection.length') === 0;
   })
