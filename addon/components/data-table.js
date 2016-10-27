9b24318141357079e32279a825aedc419751d8e2
Aad Versteden <madnificent@gmail.com>
2016-10-27 02:36:43 +0200

    Non-block version of data.table.content.header
    This variant takes the fields from the data-table and uses them to
    create a sortable set of headers.  The fields description can either be
    falsey (no headers), a string containing a space-separated set of
    properties, or an array containing the property name strings.

--- addon/components/data-table.js
+++ addon/components/data-table.js
@@ -6,5 +6,13 @@ export default Ember.Component.extend({
   noDataMessage: 'No data',
   selection: Ember.computed.filterBy('rows', 'isSelected', true),
   hasMenu: false, // set from inner component, migth fail with nested if
-  enableSelection: Ember.computed.oneWay('hasMenu')
+  enableSelection: Ember.computed.oneWay('hasMenu'),
+  parsedFields: Ember.computed('fields', function() {
+    const fields = this.get('fields');
+    if( Ember.typeOf( fields ) == 'string' ) {
+      return fields.split(' ');
+    } else {
+      return fields || []
+    }
+  }),
 });
