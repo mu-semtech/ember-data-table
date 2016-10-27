bd4d52be1c6f8123d12d8761466bae69bae9a623
Aad Versteden <madnificent@gmail.com>
2016-10-27 02:19:02 +0200

    Make table.menu optional
    When no table.menu.selected is used, the data-table will not show
    checkboxes to select items.  Making table.menu.selected optional.  If no
    actions are to be supplied, the whole table.menu block can now be
    dropped.

--- addon/components/data-table-menu-selected.js
+++ addon/components/data-table-menu-selected.js
@@ -3,6 +3,10 @@ import layout from '../templates/components/data-table-menu-selected';
 
 export default Ember.Component.extend({
   layout,
+  init: function() {
+    this._super( ...arguments );
+    this.set('data-table.enableSelection', true);
+  },
   selectionCount: Ember.computed('selection', function() {
     return this.get('selection.length');
   }),
--- addon/components/data-table.js
+++ addon/components/data-table.js
@@ -4,5 +4,7 @@ import layout from '../templates/components/data-table';
 export default Ember.Component.extend({
   layout: layout,
   noDataMessage: 'No data',
-  selection: Ember.computed.filterBy('rows', 'isSelected', true)
+  selection: Ember.computed.filterBy('rows', 'isSelected', true),
+  hasMenu: false, // set from inner component, migth fail with nested if
+  enableSelection: Ember.computed.oneWay('hasMenu')
 });
