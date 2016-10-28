25c556bfe6cb077f2ba46d1af5925712292c8d77
Aad Versteden <madnificent@gmail.com>
2016-10-28 15:56:54 +0200

    Rename rows to content
    Content is the canonical name for an array of objects which is to be
    rendered in a list.  We are moving to this naming.

--- addon/components/data-table.js
+++ addon/components/data-table.js
@@ -4,7 +4,7 @@ import layout from '../templates/components/data-table';
 export default Ember.Component.extend({
   layout: layout,
   noDataMessage: 'No data',
-  selection: Ember.computed.filterBy('rows', 'isSelected', true),
+  selection: Ember.computed.filterBy('content', 'isSelected', true),
   hasMenu: false, // set from inner component, migth fail with nested if
   enableSelection: Ember.computed.oneWay('hasMenu'),
   parsedFields: Ember.computed('fields', function() {
