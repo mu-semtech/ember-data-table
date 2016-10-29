ac311b42e6f554af12249d1f99ef0de2f2f2c072
Aad Versteden <madnificent@gmail.com>
2016-10-29 09:14:44 +0200

    Allow link to be added under first column
    When the first column of the data-table is clicked, you can now navigate
    to a different route.  This may be sufficient in many cases of data
    display.

--- addon/components/default-data-table-content-body.js
+++ addon/components/default-data-table-content-body.js
@@ -3,5 +3,14 @@ import layout from '../templates/components/default-data-table-content-body';
 
 export default Ember.Component.extend({
   layout,
-  tagName: ''
+  tagName: '',
+  allFields: Ember.computed.oneWay('data-table.parsedFields')
+  firstColumn: Ember.computed( 'data-table.parsedFields', function(){
+    return this.get('data-table.parsedFields.firstObject');
+  }),
+  otherColumns: Ember.computed( 'data-table.parsedFields', function(){
+    const fields = this.get('data-table.parsedFields');
+    return fields.splice(1);
+  }),
+  linkedRoute: Ember.computed.oneWay( 'data-table.link' ),
 });
