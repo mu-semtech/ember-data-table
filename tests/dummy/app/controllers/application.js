0bee2c026909c133ccbd3e26d299878be4ee11f3
Erika Pauwels <erika.pauwels@gmail.com>
2016-10-31 12:32:04 +0100

    Update data-table examples in the dummy app

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -3,29 +3,31 @@ import Ember from 'ember';
 var ApplicationController = Ember.Controller.extend({
   page: 2,
   size: 5,
-  links: {
-    first: {
-      number: 0,
-      size: 3
-    },
-    prev: {
-      number: 1,
-      size: 5
-    },
-    next: {
-      number: 3,
-      size: 5
-    },
-    last: {
-      number: 4,
-      size: 3
-    }
-  },
-  rows: Ember.ArrayProxy.create({
+  model: Ember.ArrayProxy.create({
     content: [
-      Ember.Object.create({ firstName: 'John', lastname: 'Doe', age: 20, created: Date.now(), modified: Date.now() }),
-      Ember.Object.create({ firstName: 'Jane', lastname: 'Doe', age: 25, created: Date.now(), modified: Date.now() })
-    ]
+      Ember.Object.create({ firstName: 'John', lastName: 'Doe', age: 20, created: Date.now(), modified: Date.now() }),
+      Ember.Object.create({ firstName: 'Jane', lastName: 'Doe', age: 25, created: Date.now(), modified: Date.now() })
+    ],
+    meta: {
+      pagination: {
+        first: {
+          number: 0,
+          size: 3
+        },
+        prev: {
+          number: 1,
+      size: 5
+        },
+        next: {
+          number: 3,
+          size: 5
+        },
+        last: {
+          number: 4,
+          size: 3
+        }
+      }
+    }
   }),
   sort: 'first-name',
   dateFormat: 'DD/MM/YYYY hh:mm:ss',
@@ -34,7 +36,7 @@ var ApplicationController = Ember.Controller.extend({
       console.log("Hi, you reached the test action for row: " + JSON.stringify(row));
     },
     menuTest() {
-      console.log("Hi, you reached the menu test action");
+      console.log("Hi, you reached the general menu test action");
     },
     selectionTest(selection) {
       selection.setEach('isSelected', false);
