18777fb80ce70f7b0c0b187755d89c7c3bcfdaa1
Erika Pauwels <erika.pauwels@gmail.com>
2016-10-31 12:30:43 +0100

    Fix syntax error

--- addon/components/default-data-table-content-body.js
+++ addon/components/default-data-table-content-body.js
@@ -4,7 +4,7 @@ import layout from '../templates/components/default-data-table-content-body';
 export default Ember.Component.extend({
   layout,
   tagName: '',
-  allFields: Ember.computed.oneWay('data-table.parsedFields')
+  allFields: Ember.computed.oneWay('data-table.parsedFields'),
   firstColumn: Ember.computed( 'data-table.parsedFields', function(){
     return this.get('data-table.parsedFields.firstObject');
   }),
