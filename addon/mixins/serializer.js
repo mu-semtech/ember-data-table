ee18de971ac84da0cd6e713fd2a901e594327dd6
Erika Pauwels <erika.pauwels@gmail.com>
2016-12-04 17:23:48 +0100

    Remove parsing of filter/sort query params
    The filter and sort query param should be parsed from the 'self' URL in
    the response, not from the pagination URLs. Since the 'self' URL is
    currently not provided, these parameters cannot be set by the serializer.

--- addon/mixins/serializer.js
+++ addon/mixins/serializer.js
@@ -19,17 +19,16 @@ export default Ember.Mixin.create({
   /**
      Transforms link URLs to objects containing metadata
      E.g.
-     links: {
+     {
          previous: '/streets?page[number]=1&page[size]=10&sort=name
          next: '/streets?page[number]=3&page[size]=10&sort=name
      }
 
      will be converted to
 
-     meta: {
+     {
          previous: { number: 1, size: 10 },
-         next: { number: 3, size: 10 },
-         sort: 'name'
+         next: { number: 3, size: 10 }
      }
    */
   createPageMeta(data) {
@@ -38,18 +37,18 @@ export default Ember.Mixin.create({
     Object.keys(data).forEach(type => {
       const link = data[type];
       meta[type] = {};
+
       let a = document.createElement('a');
       a.href = link;
+      let query = a.search.slice(1);
 
-      a.search.slice(1).split('&').forEach(pairs => {
+      query.split('&').forEach(pairs => {
         const [param, value] = pairs.split('=');
 
         if (param === 'page[number]') {
           meta[type].number = parseInt(value);
         } else if (param === 'page[size]') {
           meta[type].size = parseInt(value);
-        } else {
-          meta[param] = value;
         }
 
       });
