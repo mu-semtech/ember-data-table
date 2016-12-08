26b2b399e83fccd6ff347693e629f0d2899da526
Erika Pauwels <erika.pauwels@gmail.com>
2016-12-08 21:27:53 +0100

    Fix firstColumn in default-data-table-content-body component

--- addon/components/default-data-table-content-body.js
+++ addon/components/default-data-table-content-body.js
@@ -6,7 +6,7 @@ export default Ember.Component.extend({
   tagName: '',
   allFields: Ember.computed.oneWay('data-table.parsedFields'),
   firstColumn: Ember.computed( 'data-table.parsedFields', function(){
-    return this.get('data-table.parsedFields.firstObject');
+    return this.get('data-table.parsedFields')[0];
   }),
   otherColumns: Ember.computed( 'data-table.parsedFields', function(){
     var fields;
