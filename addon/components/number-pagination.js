d124443ce7fbd1e43df6ef224049f596f2743e25
Erika Pauwels <erika.pauwels@gmail.com>
2016-12-08 21:07:41 +0100

    Make number pagination component start from 1 instead of 0

--- addon/components/number-pagination.js
+++ addon/components/number-pagination.js
@@ -5,10 +5,20 @@ export default Ember.Component.extend({
   layout,
   range: 10,
   min: Ember.computed('links', function() {
-    return this.get('links')['first']['number'] || 0;
+    return this.get('links')['first']['number'] || 1;
   }),
   max: Ember.computed('links', function() {
-    return this.get('links')['last']['number'];
+    const max = this.get('links')['last']['number'];
+    return max ? max + 1 : max;
+  }),
+  current: Ember.computed('page', {
+    get(key) {
+      return this.get('page') ? this.get('page') + 1 : 1;
+    },
+    set(key, value) {
+      this.set('page', value - 1);
+      return value;
+    }
   }),
   hasPagination: Ember.computed('max', function() {
     return this.get('max') !== undefined;
