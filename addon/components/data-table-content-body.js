2bab4226f567b7693dc5ae6ec283bdd0b8f5d1bd
Erika Pauwels <erika.pauwels@gmail.com>
2017-08-05 23:54:54 +0200

    Fix wrappedItems computed property
    Wrapped items must be updated when content or it's elements get updated

--- addon/components/data-table-content-body.js
+++ addon/components/data-table-content-body.js
@@ -15,7 +15,7 @@ export default Ember.Component.extend({
       }
       return offset;
   }),
-  wrappedItems: Ember.computed('content', 'data-table.selection.[]', function() {
+  wrappedItems: Ember.computed('content', 'content.[]', 'data-table.selection.[]', function() {
     const selection = this.get('data-table.selection');
     return this.get('content').map(function(item) {
       return { item: item, isSelected: selection.includes(item) };
