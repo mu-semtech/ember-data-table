629d246f54504e26c3c985c11ee3d33cd3e123e6
Erika Pauwels <erika.pauwels@gmail.com>
2018-04-25 10:43:01 +0200

    Remove dependency on ember-debounced-input-helpers

--- addon/components/text-search.js
+++ addon/components/text-search.js
@@ -1,3 +1,6 @@
+import { isEqual } from '@ember/utils';
+import { cancel, debounce } from '@ember/runloop';
+import { observer } from '@ember/object';
 import { oneWay } from '@ember/object/computed';
 import Component from '@ember/component';
 import layout from '../templates/components/text-search';
@@ -9,5 +12,27 @@ export default Component.extend({
   internalValue: oneWay('filter'),
   wait: 2000,
   auto: true,
-  placeholder: 'Search'
+  placeholder: 'Search',
+  init() {
+    this._super(...arguments);
+    this.set('value', this.get('filter'));
+  },
+  onValueChange: observer('value', function() {
+    this._valuePid = debounce(this, this.get('_setFilter'), this.get('wait'));
+  }),
+  onFilterChange: observer('filter', function() {
+    // update value if filter is update manually outsite this component
+    if (!this.isDestroying && !this.isDestroyed && !isEqual(this.get('filter'), this.get('value'))) {
+      this.set('value', this.get('filter'));
+    }
+  }),
+  _setFilter() {
+    if (!this.isDestroying && !this.isDestroyed) {
+      this.set('filter', this.get('value'));
+    }
+  },
+  willDestroy() {
+    this._super(...arguments);
+    cancel(this._valuePid);
+  }
 });
