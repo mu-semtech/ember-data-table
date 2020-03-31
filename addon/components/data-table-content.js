652cc793be63b50061f6d8a688ae5b402031e4af
Fernando <ferkoni@gmail.com>
2020-03-31 16:38:31 -0400

    Allow custom table class (#21)

--- addon/components/data-table-content.js
+++ addon/components/data-table-content.js
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
+import { alias } from '@ember/object/computed';
 import layout from '../templates/components/data-table-content';
 
 export default Component.extend({
   layout,
-  classNames: ["data-table-content"]
+  classNames: ["data-table-content"],
+  tableClass: alias("data-table.tableClass"),
 });
