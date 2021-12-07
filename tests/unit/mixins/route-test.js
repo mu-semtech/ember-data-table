65ef023fbbe27989e31d269fc11e8ba996b65ca6
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-12-07 20:39:22 +0100

    Treeshake unused Lodash functions
    This replaces ember-lodash with ember-auto-import and the "real" Lodash package which makes it possible to treeshake unused  utils. The final bundle size is reduced by about 40KB (gzipped).

--- tests/unit/mixins/route-test.js
+++ tests/unit/mixins/route-test.js
@@ -1,12 +1,41 @@
-import EmberObject from '@ember/object';
-import RouteMixin from 'ember-data-table/mixins/route';
-import { module, test } from 'qunit';
+/* eslint-disable ember/no-new-mixins,ember/no-mixins */
 
-module('Unit | Mixin | route', function() {
-  // Replace this with your real tests.
-  test('it works', function(assert) {
-    let RouteObject = EmberObject.extend(RouteMixin);
-    let subject = RouteObject.create();
-    assert.ok(subject);
+import EmberObject from "@ember/object";
+import RouteMixin from "ember-data-table/mixins/route";
+import { module, test } from "qunit";
+
+module("Unit | Mixin | route", function () {
+  test("it (deep) merges the response of mergeQueryOptions method with the query param options", function (assert) {
+    assert.expect(2);
+
+    let RouteObject = EmberObject.extend(RouteMixin, {
+      modelName: "test",
+      mergeQueryOptions() {
+        return {
+          foo: "bar",
+          page: {
+            size: 5,
+          },
+        };
+      },
+    });
+
+    let mockStore = {
+      query: (modelName, queryOptions) => {
+        assert.strictEqual(modelName, "test");
+        assert.deepEqual(queryOptions, {
+          sort: "name",
+          page: {
+            size: 5,
+            number: 0,
+          },
+          foo: "bar",
+        });
+      },
+    };
+
+    let mockRoute = RouteObject.create();
+    mockRoute.store = mockStore;
+    mockRoute.model({ sort: "name", page: 0, size: 20 });
   });
 });
