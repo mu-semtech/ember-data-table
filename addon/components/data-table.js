a8ea4ae974da65f26447bb1bf286d17c65374fc8
Aad Versteden <madnificent@gmail.com>
2022-01-22 17:34:20 +0100

    Conversion of data-table component
    This conversion cannot be checked in its functionality, as we must
    convert the underlying components too in order to check the current
    setup.

--- addon/components/data-table.js
+++ addon/components/data-table.js
@@ -1,64 +1,145 @@
-import { typeOf } from '@ember/utils';
-import { observer } from '@ember/object';
-import { computed } from '@ember/object';
-import { oneWay } from '@ember/object/computed';
-import { bool } from '@ember/object/computed';
-import Component from '@ember/component';
-import layout from '../templates/components/data-table';
+import { tracked } from '@glimmer/tracking';
+import Component from '@glimmer/component';
 
-export default Component.extend({
-  init() {
-    this._super(...arguments);
-    if (this.selection === undefined) this.set('selection', []);
-  },
-  layout,
-  noDataMessage: 'No data',
-  isLoading: false,
-  lineNumbers: false,
-  searchDebounceTime: 2000,
-  enableLineNumbers: bool('lineNumbers'),
-  enableSelection: oneWay('hasMenu'),
-  selectionIsEmpty: computed.equal('selection.length', 0),
-  enableSizes: true,
-  size: 5,
-  sizeOptions: computed('size', 'sizes', 'enableSizes', function () {
+import { typeOf } from '@ember/utils';
+
+export default class DataTable extends Component {
+  @tracked _selection = undefined;
+
+  get selection() {
+    if (this._selection === undefined
+        && !('selection' in this.args))
+      return [];
+    else if (this._selection !== undefined)
+      return this._selection;
+    else
+      return this.args.selection;
+  }
+
+  set selection(newSelection) {
+    this._selection = newSelection; // also trigers dependent properties
+  }
+
+  get noDataMessage() {
+    return 'noDataMessage' in this.args
+      ? this.args.noDataMessage
+      : 'No data';
+  }
+
+  get isLoading() {
+    return this.args.isLoading;
+  }
+
+  get enableLineNumbers() {
+    return this.args.lineNumbers;
+  }
+
+  get searchDebounceTime() {
+    return 'searchDebounceTime' in this.args
+      ? this.args.searchDebounceTime
+      : 2000;
+  }
+
+  get enableLineNumbers() {
+    return this.args.enableLineNumbers;
+  }
+
+  @tracked
+  _enableSelection = undefined;
+
+  get enableSelection() {
+    return this._enableSelection === undefined
+      ? this.args.hasMenu
+      : this._enableSelection;
+  }
+
+  set enableSelection(value) {
+    this._enableSelection = value;
+  }
+
+  get selectionIsEmpty() {
+    return this.selection.length === 0;
+  }
+
+  get enableSizes() {
+    return 'enableSizes' in this.args
+      ? this.args.enableSizes
+      : true;
+  }
+
+  _size = undefined;
+  get size() {
+    if (this._size === undefined && this.args.size)
+      return this.args.size;
+    else if (this._size)
+      return this._size;
+    else
+      return 5;
+  }
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  get sizeOptions() {
     if (!this.enableSizes) {
       return null;
     } else {
-      const sizeOptions = this.sizes || [5, 10, 25, 50, 100];
+      const sizeOptions = this.args.sizes || [5, 10, 25, 50, 100];
       if (!sizeOptions.includes(this.size)) {
         sizeOptions.push(this.size);
       }
       sizeOptions.sort((a, b) => a - b);
       return sizeOptions;
     }
-  }),
-  hasMenu: false, // set from inner component, migth fail with nested if
-  enableSearch: computed('filter', function () {
-    return this.filter || this.filter === '';
-  }),
-  autoSearch: true,
-  filterChanged: observer('filter', function () {
-    this.set('page', 0);
-  }),
-  sizeChanged: observer('size', function () {
-    this.set('page', 0);
-  }),
-  parsedFields: computed('fields', function () {
+  }
+
+  @tracked hasMenu = false; // old comment: set from inner component, migth fail with nested if
+
+  get enableSearch() {
+    return 'filter' in this.args;
+  }
+
+  get autoSearch() {
+    return 'autoSearch' in this.args
+      ? this.args.autoSearch
+      : true;
+  }
+
+  // TODO: make sure onFilterChanged is called and pushes its values through
+  onFilterChanged() {
+    this.page = 0;
+  }
+
+  // TODO: make sure onSizeChanged is called and pushes its values through
+  onSizeChanged() {
+    this.page = 0;
+  }
+
+  get parsedFields() {
     const fields = this.fields;
     if (typeOf(fields) === 'string') {
       return fields.split(' ');
     } else {
       return fields || [];
     }
-  }),
+  }
+
+  // filterChanged: observer('filter', function () {
+  //   this.set('page', 0);
+  // }),
+  // sizeChanged: observer('size', function () {
+  //   this.set('page', 0);
+  // }),
+  // parsedFields: computed('fields', function () {
+  // }),
+
   addItemToSelection(item) {
-    this.selection.addObject(item);
-  },
+    this.selection = [item, ...this.selection];
+  }
   removeItemFromSelection(item) {
-    this.selection.removeObject(item);
-  },
+    this.selection = this.selection.filter((x) => x !== item);
+  }
   clearSelection() {
-    this.selection.clear();
-  },
-});
+    this.selection = [];
+  }
+}
